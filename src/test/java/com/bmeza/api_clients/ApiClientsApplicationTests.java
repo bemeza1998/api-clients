e5a54761ad6995aa2bb8bf284caf29a15341f117
bemeza <esteban-meza85@hotmail.com>
2024-10-11 20:13:05 -0500

    First commit

---
+++ src/test/java/com/bmeza/api_clients/ApiClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmeza.api_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
